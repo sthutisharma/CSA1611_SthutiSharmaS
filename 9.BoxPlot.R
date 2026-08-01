095b0d28be022add66a41395c8fade8b52880270
sthutisharma <sthutisharma06@gmail.com>
2026-08-01 20:42:45 +0530

    Add files via upload

---
+++ 9.BoxPlot.R
@@ -0,0 +1,8 @@
+data(mtcars)
+head(mtcars)
+boxplot(mpg ~ cyl,data=mtcars,
+        main="MPG vs Number of Cylinders",
+        xlab="Number of cylinders",
+        ylab="MPG",
+        col=c("lightblue","lightgreen","lightpink")
+        )
