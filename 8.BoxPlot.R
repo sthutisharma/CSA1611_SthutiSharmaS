bb6e6474eb859c441bc21544bc1ce4c1d40c088d
sthutisharma <sthutisharma06@gmail.com>
2026-08-13 10:22:37 +0530

    Delete 8.BoxPlot.R

--- 8.BoxPlot.R
+++
@@ -1,8 +0,0 @@
-data(mtcars)
-head(mtcars)
-boxplot(mpg ~ cyl,data=mtcars,
-        main="MPG vs Number of Cylinders",
-        xlab="Number of cylinders",
-        ylab="MPG",
-        col=c("lightblue","lightgreen","lightpink")
-        )
