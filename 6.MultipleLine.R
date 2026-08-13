cc78280d6d5ad73dbd40beb869a42102e6ede579
sthutisharma <sthutisharma06@gmail.com>
2026-08-13 10:23:06 +0530

    Delete 6.MultipleLine.R

--- 6.MultipleLine.R
+++
@@ -1,8 +0,0 @@
-plot(mtcars$mpg,type="l",col="blue",
-     xlab="Car Index", 
-     ylab="Values")
-lines(mtcars$qsec,col="red")
-legend("topright",
-       legend=c("mpg","qsec"),
-       col=c("blue","red"),
-       lty=1)
